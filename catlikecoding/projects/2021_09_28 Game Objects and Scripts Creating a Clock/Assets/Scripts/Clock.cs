096fabc7517698da023362f8ec90031a8143feba
inexts1996 <inexts0618@gmail.com>
2021-09-29 18:53:29 +0800

    完善clock

---
+++ catlikecoding/projects/2021_09_28 Game Objects and Scripts Creating a Clock/Assets/Scripts/Clock.cs
@@ -0,0 +1,13 @@
+
+using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+   [SerializeField]
+   private Transform hourPivot, minutesPivot, secondsPivot;
+
+   private void Awake()
+   {
+   }
+}
